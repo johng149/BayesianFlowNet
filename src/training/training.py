c5d4fe59d624f1649a134dc82307c65258384c75
johng149 <johnguo149@gmail.com>
2025-07-21 20:35:58 -0700

    Basic accelerate setup

--- src/training/training.py
+++ src/training/training.py
@@ -6,21 +6,21 @@ from src.datasets.discrete_helper import beta_t, y_distribution, theta
 def sample_t(batch_size, min_t=1e-6):
    return torch.clamp(torch.FloatTensor(batch_size).uniform_(0,1), min=min_t)
 
-def train_discrete_model(model, optimizer, train_dl, epochs, device, loss_tracker=[]):
+def train_discrete_model(model, optimizer, train_dl, epochs, accelerator, loss_tracker=[]):
     model.train()
     pbar = tqdm(range(epochs), desc="Training Discrete Model")
     for epoch in pbar:
         optimizer.zero_grad()
-        ground_truth = next(train_dl).to(device)
+        ground_truth = next(train_dl).to(accelerator.device)
         batch_size, seq_len, K = ground_truth.shape
-        t = sample_t(batch_size).to(device)
+        t = sample_t(batch_size).to(accelerator.device)
         beta_1 = torch.ones_like(t) * 4
-        beta = beta_t(beta_1, t)
+        beta = beta_t(beta_1, t).to(accelerator.device)
         y_prime = y_distribution(beta, K, ground_truth)
         model_input = theta(y_prime)
         output = model.forward(model_input, t)
         l = loss(beta_1, t, ground_truth, model_output_logits=output)
-        l.backward()
+        accelerator.backward(l)
         optimizer.step()
         pbar.set_postfix({"loss": l.item()})
         loss_tracker.append(l.item())
--- train_script.py
+++ train_script.py
@@ -5,8 +5,9 @@ from src.nn.models.discrete_model import DiscreteModel
 from src.training.training import train_discrete_model
 from matplotlib import pyplot as plt
 from src.inference.discrete_inference import dis_t, bayesian_inference
+from accelerate import Accelerator
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+accelerator = Accelerator()
 tokenizer = DiscreteSyntheticTokenizer()
 max_seq_len = 32
 train_ds = DiscreteSyntheticDataset(tokenizer, tokenized_length=max_seq_len)
@@ -20,11 +21,13 @@ def make_infinite(dl):
 train_dl = make_infinite(train_dl)
 
 model = DiscreteModel(max_seq_len, tokenizer.vocab_size(), hidden_dim=64, num_heads=8)
-model = model.to(device)
 opt = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+model, opt, train_dl = accelerator.prepare(model, opt, train_dl)
+
 loss_tracker = []
 
-train_discrete_model(model, opt, train_dl, epochs=10_000, device=device, loss_tracker=loss_tracker)
+train_discrete_model(model, opt, train_dl, epochs=1_000, accelerator=accelerator, loss_tracker=loss_tracker)
 
 # plot loss to file
 plt.plot(loss_tracker)
@@ -34,7 +37,7 @@ plt.title('Training Loss')
 plt.savefig('training_loss.png')
 plt.show()
 
-model_input = torch.normal(0, 1, (1, max_seq_len, tokenizer.vocab_size())).to(device)
+model_input = torch.normal(0, 1, (1, max_seq_len, tokenizer.vocab_size())).to(accelerator.device)
 model_input = torch.softmax(model_input, dim=-1) * 2 - 1
 
 n = 190
@@ -42,10 +45,10 @@ n = 190
 for i in range(1, n+1):
   if i % 10 == 0:
     tokenizer.decode(model_input.squeeze(0))
-  cur_it = torch.tensor([i], device=device)
-  total_it = torch.tensor([n], device=device)
-  t = dis_t(cur_it,total_it).to(device)
-  dis_beta_1 = torch.ones_like(t, device=device) * 4
+  cur_it = torch.tensor([i], device=accelerator.device)
+  total_it = torch.tensor([n], device=accelerator.device)
+  t = dis_t(cur_it,total_it).to(accelerator.device)
+  dis_beta_1 = torch.ones_like(t, device=accelerator.device) * 4
   model_output = model.forward(model_input, t)
   model_input = bayesian_inference(model_input, model_output, cur_it, total_it, dis_beta_1)
 
