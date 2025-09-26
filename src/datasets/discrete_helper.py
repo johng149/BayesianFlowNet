c7e4666af24d2b1fb824b77c56e0973a20c8275c
johng149 <johnguo149@gmail.com>
2025-09-25 20:20:35 -0700

    Add epsilon to beta_t to prevent beta from being 0 at time t=0

--- src/datasets/discrete_helper.py
+++ src/datasets/discrete_helper.py
@@ -2,11 +2,13 @@ import torch
 from torch import Tensor
 from torch.nn import functional as F
 
-def beta_t(beta_1: Tensor, t: Tensor) -> Tensor:
+
+def beta_t(beta_1: Tensor, t: Tensor, eps: float = 1e-8) -> Tensor:
     """
     Args:
         beta_1: Maximum possible accuracy (reached when t=1) of shape (batch_size,).
         t: A tensor representing the time step, where 1 corresponds to maximum accuracy of shape (batch_size,).
+        eps: Small value to prevent beta at t=0 from being exactly 0.
     Returns:
         Beta value at given time step t
     """
@@ -15,7 +17,8 @@ def beta_t(beta_1: Tensor, t: Tensor) -> Tensor:
     assert beta_1.shape == t.shape, "beta_1 and t should have the same shape"
     assert torch.all(t >= 0), "t must be at least 0"
     assert torch.all(t <= 1), "t must be at most 1"
-    return beta_1 * (t ** 2)
+    return beta_1 * (t**2) + eps
+
 
 def y_distribution(beta: Tensor, K: int, kron_x: Tensor) -> Tensor:
     """
@@ -27,11 +30,16 @@ def y_distribution(beta: Tensor, K: int, kron_x: Tensor) -> Tensor:
         Noisy version of kron_x with the amount of noise controlled
         by beta. The shape of the output tensor is the same as kron_x, i.e., (batch_size, seq_len, K).
     """
-    beta = beta.view(-1, 1, 1) # allows for broadcasting with reach appropriate batch in kron_x
+    beta = beta.view(
+        -1, 1, 1
+    )  # allows for broadcasting with reach appropriate batch in kron_x
     mean = beta * (K * kron_x - 1)
     variance = beta * K
     epsilon = torch.normal(0, 1, kron_x.shape, device=kron_x.device)
-    return mean + variance * epsilon # I know the name `variance` suggests it should be squared, but this works just fine
+    return (
+        mean + variance * epsilon
+    )  # I know the name `variance` suggests it should be squared, but this works just fine
+
 
 def theta(y: Tensor):
     """
@@ -45,8 +53,10 @@ def theta(y: Tensor):
     theta = 2 * theta - 1  # scale to [-1, 1]
     return theta
 
+
 def sample_t(batch_size, min_t=1e-6):
-   return torch.clamp(torch.FloatTensor(batch_size).uniform_(0,1), min=min_t)
+    return torch.clamp(torch.FloatTensor(batch_size).uniform_(0, 1), min=min_t)
+
 
 def collate_fn(batch):
     """
@@ -60,21 +70,18 @@ def collate_fn(batch):
         (batch_size, seq_len, K), 't' is a tensor of shape (batch_size,), 'beta_1'
         is a tensor of shape (batch_size,), and 'theta' is the transformed version of 'x'.
     """
-    x = [item['x'] for item in batch]
+    x = [item["x"] for item in batch]
     min_length = min(seq.shape[0] for seq in x)
     x = [tensor[:min_length] for tensor in x]
 
     x = torch.stack(x, dim=0)  # Shape: (batch_size, seq_len, K)
-    t = torch.cat([item['t'] for item in batch], dim=0)  # Shape: (batch_size,)
-    beta = torch.cat([item['beta'] for item in batch], dim=0)
-    beta_1 = torch.cat([item['beta_1'] for item in batch], dim=0)  # Shape: (batch_size,)
+    t = torch.cat([item["t"] for item in batch], dim=0)  # Shape: (batch_size,)
+    beta = torch.cat([item["beta"] for item in batch], dim=0)
+    beta_1 = torch.cat(
+        [item["beta_1"] for item in batch], dim=0
+    )  # Shape: (batch_size,)
 
     y = y_distribution(beta, x.shape[-1], x)  # Shape: (batch_size, seq_len, K)
     theta_tensor = theta(y)  # Shape: (batch_size, seq_len, K)
 
-    return {
-        'x': x,
-        't': t,
-        'beta_1': beta_1,
-        'theta': theta_tensor
-    }
+    return {"x": x, "t": t, "beta_1": beta_1, "theta": theta_tensor}
